6fa0028156b311510052c4c2ddead4dbe7d953ab
andland <andland@gmail.com>
2013-04-10 21:13:08 -0300

    Create sparse_logistic_pca.R

---
+++ R/sparse_logistic_pca.R
@@ -0,0 +1,58 @@
+inv.logit.mat <- function(x, min = 0, max = 1) {
+  p <- exp(x)/(1 + exp(x))
+  which.large=is.na(p) & !is.na(x)
+  p[which.large]=1
+  p * (max - min) + min
+}
+
+sparse.logistic.pca <- function(dat,lambda=0,k=2,quiet=TRUE,max.iters=100) {
+  # From Lee, Huang, Hu (2010)
+  # Does not deal with missing data
+  # Uses the uniform bound for the log likelihood
+  q=as.matrix(2*dat-1)
+  n=nrow(dat)
+  d=ncol(dat)
+  
+  # Initialize #
+  ##################
+  mu=rnorm(d)
+  A=svd(scale(dat,center=TRUE,scale=FALSE))$u[,1:k]
+  B=svd(scale(dat,center=TRUE,scale=FALSE))$v[,1:k] %*% diag(svd(scale(dat,center=TRUE,scale=FALSE))$d[1:k])
+  # row.names(A)=row.names(dat); row.names(B)=colnames(dat)
+  loss.trace=numeric(max.iters)
+  
+  for (m in 1:max.iters) {
+    theta=outer(rep(1,n),mu)+A %*% t(B)
+    X=as.matrix(theta+4*q*(1-inv.logit.mat(q*theta)))
+    Xcross=X-A %*% t(B)
+    mu=as.numeric(1/n*t(Xcross) %*% rep(1,n))
+    
+    theta=outer(rep(1,n),mu)+A %*% t(B)
+    X=as.matrix(theta+4*q*(1-inv.logit.mat(q*theta)))
+    Xstar=X-outer(rep(1,n),mu)
+    A=Xstar %*% B %*% solve(t(B) %*% B)
+    A=qr.Q(qr(A))
+    
+    theta=outer(rep(1,n),mu)+A %*% t(B)
+    X=as.matrix(theta+4*q*(1-inv.logit.mat(q*theta)))
+    Xstar=X-outer(rep(1,n),mu)
+    C=t(Xstar) %*% A
+    B=abs(B)/(abs(B)+4*n*lambda)*C
+    
+    loglike=sum(log(inv.logit.mat(q*(outer(rep(1,n),mu)+A %*% t(B)))))
+    penalty=n*lambda*sum(abs(B))
+    loss.trace[m]=-loglike+penalty
+    
+    if (!quiet) 
+      cat(m,"  ",zapsmall(-loglike),"   ",zapsmall(penalty),"     ",-loglike+penalty, "\n")
+    
+    if (m>15) {
+      if ((loss.trace[m-1]-loss.trace[m])<(.0001)) 
+        break
+    }
+    
+  }
+  zeros=sum(abs(B)<1e-10)
+  BIC=-2*loglike+log(n)*(d+n*k+sum(abs(B)>=1e-10))
+  return(list(mu=mu,A=A,B=B,zeros=zeros,BIC=BIC,iters=m,loss.trace=loss.trace[1:m]))
+}
